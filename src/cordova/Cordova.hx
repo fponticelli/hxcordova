4c06b29d42861873d965651bd3d96535829d1cfe
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-25 10:24:24 -0600

    added requestFileSystem methods

--- src/cordova/Cordova.hx
+++ src/cordova/Cordova.hx
@@ -1,9 +1,18 @@
 package cordova;
 
 import js.Browser.*;
+import cordova.file.*;
 
 class Cordova {
-  public static function onDeviceReady(handler : Void -> Void) {
+  public static function onDeviceReady(handler : Void -> Void)
     document.addEventListener("deviceready", handler, false);
-  }
+
+  inline public static function requestFileSystem(type : String, size : Int, handler : FileSystem -> Void, error : Dynamic -> Void)
+    (untyped window.requestFileSystem)(type, size, handler, error);
+
+  inline public static function requestTemporaryFileSystem(size : Int, handler : FileSystem -> Void, error : Dynamic -> Void)
+    return requestFileSystem(LocalFileSystem.TEMPORARY, size, handler, error);
+
+  inline public static function requestPermanentFileSystem(size : Int, handler : FileSystem -> Void, error : Dynamic -> Void)
+    return requestFileSystem(LocalFileSystem.PERMANENT, size, handler, error);
 }
