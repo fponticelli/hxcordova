4a86a6a1abe08e1241d848324096719f0811951c
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-25 20:46:13 -0600

    added FileError

---
+++ src/cordova/file/FileError.hx
@@ -0,0 +1,16 @@
+package cordova.file;
+
+extern class FileError {
+  static var NOT_FOUND_ERR(default, null) : Int;
+  static var SECURITY_ERR(default, null) : Int;
+  static var ABORT_ERR(default, null) : Int;
+  static var NOT_READABLE_ERR(default, null) : Int;
+  static var ENCODING_ERR(default, null) : Int;
+  static var NO_MODIFICATION_ALLOWED_ERR(default, null) : Int;
+  static var INVALID_STATE_ERR(default, null) : Int;
+  static var SYNTAX_ERR(default, null) : Int;
+  static var INVALID_MODIFICATION_ERR(default, null) : Int;
+  static var QUOTA_EXCEEDED_ERR(default, null) : Int;
+  static var TYPE_MISMATCH_ERR(default, null) : Int;
+  static var PATH_EXISTS_ERR(default, null) : Int;
+}
