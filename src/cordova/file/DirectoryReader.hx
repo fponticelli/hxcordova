8e803022774912880f3d391ea05ca8874e59694a
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-25 20:47:50 -0600

    readers and build helper

---
+++ src/cordova/file/DirectoryReader.hx
@@ -0,0 +1,6 @@
+package cordova.file;
+
+//@:native("cordova.file.FileSystem")
+extern class DirectoryReader {
+
+}
---
+++ src/cordova/file/FileReader.hx
@@ -0,0 +1,21 @@
+package cordova.file;
+
+//@:native("cordova.file.FileReader")
+extern class FileReader {
+  var readyState : ReadyState;
+  var result : String;
+  var error : Int; // FileError code
+  var onloadstart : Dynamic -> Void;
+  var onload : Dynamic -> Void;
+  var onabort : Dynamic -> Void;
+  var onerror : Dynamic -> Void;
+  var onloadend : Dynamic -> Void;
+
+  function new() : Void;
+  function abort() : Void;
+  function readAsDataURL(entry : FileEntry) : Void;
+  function readAsArrayBuffer(entry : FileEntry) : Void;
+  function readAsBinaryString(entry : FileEntry) : Void;
+  function readAsText(entry : FileEntry, ?encoding : String) : Void;
+  var onloadended : Dynamic -> Void; // TODO type event correctly
+}
