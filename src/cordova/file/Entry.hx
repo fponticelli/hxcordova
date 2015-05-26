096c146aaa2e04cba741e0147e8612c6c9f2a592
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-25 20:47:27 -0600

    base file/directory entry

---
+++ src/cordova/file/Entry.hx
@@ -0,0 +1,21 @@
+package cordova.file;
+
+//@:native("cordova.file.FileEntry")
+extern class Entry {
+  var isFile(default, never) : Bool;
+  var isDirectory(default, never) : Bool;
+  var name(default, never) : String;
+  var fullPath(default, never) : String;
+
+  function getMetadata(handler : Metadata -> Void, error : Dynamic -> Void) : Void;
+  function moveTo(destination : DirectoryEntry, ?newFileName : String, handler : FileEntry -> Void, error : Dynamic -> Void) : Void;
+  function copyTo(destination : DirectoryEntry, ?newFileName : String, handler : FileEntry -> Void, error : Dynamic -> Void) : Void;
+  function toURL() : String;
+  function remove(handler : FileEntry -> Void, error : Dynamic -> Void) : Void;
+  function getParent() : DirectoryEntry;
+}
+
+typedef EntryOptions = {
+  create : Bool,
+  exclusive : Bool
+}
