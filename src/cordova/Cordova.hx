747bfe8bcf68413ff939efd6dcf6af782b936ad1
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-26 23:13:04 -0600

    fixed reference to LocalFileSystem

--- src/cordova/Cordova.hx
+++ src/cordova/Cordova.hx
@@ -10,14 +10,14 @@ class Cordova {
   public static function onDeviceReady(handler : Void -> Void)
     document.addEventListener("deviceready", handler, false);
 
-  inline public static function requestFileSystem(type : String, size : Int, handler : FileSystem -> Void, error : Dynamic -> Void)
+  inline public static function requestFileSystem(type : Int, size : Int, handler : FileSystem -> Void, error : Dynamic -> Void)
     (untyped window.requestFileSystem)(type, size, handler, error);
 
   inline public static function requestTemporaryFileSystem(size : Int, handler : FileSystem -> Void, error : Dynamic -> Void)
     return requestFileSystem(LocalFileSystem.TEMPORARY, size, handler, error);
 
-  inline public static function requestPermanentFileSystem(size : Int, handler : FileSystem -> Void, error : Dynamic -> Void)
-    return requestFileSystem(LocalFileSystem.PERMANENT, size, handler, error);
+  inline public static function requestPersistentFileSystem(size : Int, handler : FileSystem -> Void, error : Dynamic -> Void)
+    return requestFileSystem(LocalFileSystem.PERSISTENT, size, handler, error);
 
   inline public static function resolveLocalFileSystemURL(path : String, handler : FileEntry -> Void, error : Dynamic -> Void)
     (untyped window.resolveLocalFileSystemURL)(path, handler, error);
